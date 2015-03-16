d803fa1ba3a473f6bd4732c6ae03fd009a542921
JackHillman <jack.hillman@me.com>
2015-03-16 17:25:43 +1030

    changed tagling location

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
 var text = ["We are Visceral Books", "We <span>live</span> for the stories.", "We <span>love</span> the drama.", "We <span>crave</span> the success.", "We <span>feel</span> the romance."];
 
-$("h5").html("We are Visceral Books");
+$(".title").html("We are Visceral Books");
 
 	var x = false;
 
 	function myLoop(i) {          
 	   setTimeout(function () {   
-		    $("h5").html(text[i-1]).fadeTo(250, 1).delay(2500);
+		    $(".title").html(text[i-1]).fadeTo(250, 1).delay(2500);
 			if (i != 1) {
-				$("h5").fadeTo(250, 0);
+				$(".title").fadeTo(250, 0);
 			}
 
 			console.log(i);
@@ -19,7 +19,7 @@ $("h5").html("We are Visceral Books");
 	   }, 3000)
 	};
 
-	$("h5").delay(2750).fadeTo(250, 0);
+	$(".title").delay(2750).fadeTo(250, 0);
 	myLoop(text.length);
 
 });
